8e8c94f787c28d3a75e9aa8f18aabfadfc75368e
ArnoldasTumenas <117638978+ArnoldasTumenas@users.noreply.github.com>
2023-04-01 18:28:42 +0300

    Assignment

--- File-manager/index.php
+++ File-manager/index.php
@@ -81,7 +81,7 @@ unset($data[1]);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>File-anager</title>
+    <title>File-manager</title>
 </head>
 
 <body>
